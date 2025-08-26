2d38964d37059da63013aeb83421b3dc0daa5c5f
barkha2596 <barkhabandana02@gmail.com>
2025-08-26 23:08:00 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ app.use("/hello",(req,res) => {
     res.send("Hello from the server and from nodemon!!!!!")
 })
 
+app.get("/user/:userId/:name",(req,res) => {
+    console.log(req.params)
+    const {userId , name} =req.params;
+    console.log(userId);
+    console.log(name)
+    res.send({firstName: "Barkha", secondName: "Bandana"})
+})
 
 app.listen(8080, () => {
     console.log("server is successfully listening")
