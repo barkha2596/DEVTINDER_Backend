eed6c3459de5ea3b21507cb0e56f595bcd238ffe
barkha2596 <barkhabandana02@gmail.com>
2025-08-27 01:52:21 +0530

    error Handling

--- src/app.js
+++ src/app.js
@@ -2,23 +2,27 @@ const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const app= express();
 
-app.use("/admin",adminAuth)
 
-app.get("/user/login", (req,res) => {
-    res.send("login successful")
+app.get("/getUserData",(req,res) => {
+    // try{
+        throw new Error("hfregfyrefguyr")
+        res.send("User Data Received");
+    // }catch(err){
+        //res.status(500).send("Some error occured, contact support team")
+   // }
 })
 
-app.get("/user/getData", userAuth, (req,res) =>{
-    res.send("User Data received")
+app.use("/", (err, req, res, next) =>{
+    if(err){
+        res.status(500).send("Something went wrong");
+    }
 })
 
-app.get("/admin/getAllData",(req,res) => {
-    res.send("User Data received")
-})
 
-app.get("/admin/deleteAllData",(req,res) => {
-    res.send("User Data Deleted!!")
-})
+
+
+
+
 
 app.listen(8080, () => {
     console.log("server is successfully listening")
