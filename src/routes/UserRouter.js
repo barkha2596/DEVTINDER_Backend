1cd5153443187a7cb2bb5f238bd7a71cf7b9dc68
barkha2596 <barkhabandana02@gmail.com>
2025-09-03 15:38:40 +0530

    feed api and pagination

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -50,6 +50,44 @@ userRouter.get("/user/connection", userAuth, async(req,res) => {
     }
 })
 
+userRouter.get("/user/feed", userAuth, async(req,res) =>{
+    try{
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit= limit>50 ?50:limit;
+        const skip = (page-1)*limit;
+        const connectionRequests = await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUserFromFeed = new Set();
+        connectionRequests.forEach((req) =>{
+            hideUserFromFeed.add(req.toUserId.toString());
+            hideUserFromFeed.add(req.fromUserId.toString());
+        });
+
+        const users = await UserActivation.find({
+            $and: [
+                {_id: {$nin: Array.from(hideUserFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ].select(USER_SAFE_DATA)
+             .skip(skip)
+             .limit(limit)
+        })
+        res.json({
+            data:users
+        })
+    }catch(err){
+        res.status(400).json({
+            message: err.message
+        })
+    }
+})
+
 
 
 
